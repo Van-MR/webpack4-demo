e0fe35e3687e0afbde156b459ac260204cd56d43
vanisher <vannisher@yahoo.com>
2019-01-17 10:43:47 +0800

    css tree snaking

---
+++ demo09/webpack.config.js
@@ -0,0 +1,60 @@
+/**
+ * 1. webpack4下安装extract-text-webpack-plugin: npm install --save-dev extract-text-webpack-plugin@next
+ * 2. CSS Tree Shaking: './src/css/base.css'中没有被使用的css样式不会被打包, 例如, .box-small
+ */
+
+const path = require('path');
+const PurifyCSS = require("purifycss-webpack");
+const glob = require("glob-all");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+let ExtractTextPlugin = new ExtractTextPlugin({
+    filename:"[name].min.css",
+    allChunks:false
+});
+
+
+let purifyCSS = new PurifyCSS({
+    path: glob.sync([
+        path.resolve(__dirname,'./*.html'),
+        path.resolve(__dirname,'./src/*.js')
+    ])
+}};
+
+
+
+module.exports = {
+    entry:{
+      app:'./src/app.js'
+    },
+    output:{
+      publicPath:__dirname + "/dist/",
+      path:path.resolve(__dirname,"dist"),
+      filename:"[name].bundle.js",
+      chunkFilename:"[name].chunk.js"
+    },
+    module{
+       rules:[
+         {
+           test:/\.css$/,
+           use:ExtractTextPlugin.extract({
+               fallback: {
+                   loader:"style-loader",
+                   options: {
+                       singleton: true
+                   }
+               },
+               use:{
+                 loader:"css-loader",
+                 options:{
+                   minimize: true
+                 }
+               }
+           }}
+         }
+       ]
+    },
+    plugins:[ExtractTextPlugin,purifyCSS]
+
+};
